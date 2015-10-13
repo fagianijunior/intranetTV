585b6a2c412aa46f0e3115235e382e73f70b0600
Carlos Fagiani Junior <fagianijunior@gmail.com>
2015-10-13 14:14:36 +0000

    js

--- app/assets/javascripts/uniform_pieces.js
+++ app/assets/javascripts/uniform_pieces.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+  function PrintDiv() {    
+    var divToPrint = document.getElementById('divToPrint');
+    var popupWin = window.open('', '_blank', 'width=800,height=600');
+    popupWin.document.open();
+    popupWin.document.write('<html><body onload="window.print()">' + divToPrint.innerHTML + '</html>');
+    popupWin.document.close();
+  }
