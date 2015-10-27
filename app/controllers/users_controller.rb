26b15096f2d420292262f7c687c6aa2247b6e363
Carlos Fagiani Junior <fagianijunior@gmail.com>
2015-10-27 11:37:53 +0000

    aceitar role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < AdminController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :employer_id)
+      params.require(:user).permit(:email, :password, :password_confirmation, :employer_id, :role)
     end
 end
